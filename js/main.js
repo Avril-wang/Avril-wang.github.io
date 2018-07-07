b16e42b830a8ac2f148b844d2d1096da7b9ef036
Avril-wang <wzldwbo@163.com>
2018-07-07 22:17:14 +0200

    d3 svg animation chart

--- js/main.js
+++ js/main.js
@@ -1,43 +1,84 @@
-var data = [20,141,50,100,80];
-var svg_height = 220;
-var svg_width = 1000;
-var d = svg_width / data.length;
-//比例尺
-var scale = d3.scaleLinear().domain([0, d3.max(data)]).range([0, svg_height]);
-//折线生成函数
-var line_generator = function(x,i) {
-  var offset = -1;
-  var line_data = data.map( function(y) {
-    offset ++;
-    return { y: svg_height - scale(y), x: offset * d };
-  } );
-  var lineFunction = d3.line().x(function(d){return d.x;}).y(function(d){return d.y;});
-  return lineFunction(line_data);
-};
-//区域生成函数
-var area_generator = function(x,i) {
-  var offset = -1;
-  var area_data = data.map( function(y) {
-    offset ++;
-    return [offset * d, svg_height - scale(y)];
-  } );
-  var area = d3.area().y0(svg_height);
-  return area(area_data);
-};
-setTimeout( function() {
-  // line
-  d3.select('#chart')
-    .data(data)         //数据驱动：根据data的长度去生成对应的path
-    .append('path')
-      .attr('stroke', 'rgb(66, 132, 243)')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none')
-      .attr('d', line_generator);
-  // area
-  d3.select('#chart')
-    .data(data)
-    .append('path')
-      .attr('stroke', 'none')
-      .attr('fill', 'rgba(66, 132, 243, 0.2)')
-      .attr('d', area_generator);
-  }, 300 );
+var line = d3.line();
+
+var data0 =[
+    {x:0,y:0},
+    {x:350, y:0},
+    {x:650, y:0},
+    {x:950, y:0},
+];
+
+var data1 =[
+    {x:0,y:0},
+    {x:350, y:100},
+    {x:650, y:50},
+    {x:950, y:80},
+];
+
+var data2 =[
+    {x:0,y:100},
+    {x:350, y:80},
+    {x:650, y:20},
+    {x:950, y:40},
+];
+
+var data3 =[
+    {x:0,y:20},
+    {x:350, y:29},
+    {x:650, y:90},
+    {x:950, y:190},
+];
+
+var data4 =[
+    {x:0,y:200},
+    {x:350, y:4},
+    {x:650, y:100},
+    {x:950, y:2},
+];
+
+var p = d3.select('#path1');
+
+var area = d3.area()
+    .x(function(d){return d.x;})
+    .y0(0)
+    .y1(function(d){return d.y;});
+
+d3.select('#path1')
+.attr('d', line(data1))
+.attr('d',area(data1))
+.transition()
+
+d3.select('#run1').on('mouseover',function(){
+    d3.select('#path1')
+    .attr('d', line(data1))
+    .attr('d',area(data1))
+    .transition()
+    .duration(500)
+}) 
+
+d3.select('#run2').on('mouseover',function(){
+    d3.select('#path1')
+    .attr('d', line(data2))
+    .attr('d',area(data2))
+    .transition()
+    .duration(500)
+}) 
+
+d3.select('#run3').on('mouseover',function(){
+    d3.select('#path1')
+    .attr('d', line(data3))
+    .attr('d',area(data3))
+    .transition()
+    .duration(500)
+}) 
+
+d3.select('#run4').on('mouseover',function(){
+    d3.select('#path1')
+    .attr('d', line(data4))
+    .attr('d',area(data4))
+    .transition()
+    .duration(500)
+}) 
+
+
+
+
