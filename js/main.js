7c343df23e0585dcf3ab47efd5e73de80e138b35
Avril-wang <wzldwbo@163.com>
2018-07-09 18:12:54 +0200

    add geely page

--- js/main.js
+++ js/main.js
@@ -1,41 +1,45 @@
 var line = d3.line();
 
 var data0 =[
-    {x:0,y:0},
-    {x:350, y:0},
-    {x:650, y:0},
-    {x:950, y:0},
+    {"x":0,"y":0,"name":"time"},
+    {"x":350, "y":0,"name":"Language"},
+    {"x":650, "y":0,"name":"Skill"},
+    {"x":950, "y":0,"name":"Horizen"},
 ];
 
 var data1 =[
-    {x:0,y:0},
-    {x:350, y:100},
-    {x:650, y:50},
-    {x:950, y:80},
+    {"x":0,"y":0,"name":"time"},
+    {"x":350, "y":100,"name":"Language"},
+    {"x":650, "y":50,"name":"Skill"},
+    {"x":950, "y":80,"name":"Horizen"},
 ];
 
 var data2 =[
-    {x:0,y:100},
-    {x:350, y:80},
-    {x:650, y:20},
-    {x:950, y:40},
+    {"x":0,"y":100,"name":"time"},
+    {"x":350, "y":80,"name":"Language"},
+    {"x":650, "y":20,"name":"Skill"},
+    {"x":950, "y":40,"name":"Horizen"},
 ];
 
 var data3 =[
-    {x:0,y:20},
-    {x:350, y:29},
-    {x:650, y:90},
-    {x:950, y:190},
+    {"x":0,"y":20,"name":"time"},
+    {"x":350, "y":29,"name":"Language"},
+    {"x":650, "y":90,"name":"Skill"},
+    {"x":950, "y":190,"name":"Horizen"},
 ];
 
 var data4 =[
-    {x:0,y:200},
-    {x:350, y:4},
-    {x:650, y:100},
-    {x:950, y:2},
+    {x:0,y:200,name:"time"},
+    {x:350, y:4,name:"Language"},
+    {x:650, y:100,name:"Skill"},
+    {x:950, y:2,name:"Horizen"},
 ];
 
-var p = d3.select('#path1');
+var myTool = d3.select('#path1')
+             .append('div')
+             .attr("class","mytooltip")
+             .style("opacity","1")
+             .style("display","block");
 
 var area = d3.area()
     .x(function(d){return d.x;})
@@ -43,8 +47,8 @@ var area = d3.area()
     .y1(function(d){return d.y;});
 
 d3.select('#path1')
-.attr('d', line(data1))
-.attr('d',area(data1))
+.attr('d', line(data0))
+.attr('d',area(data0))
 .transition()
 
 d3.select('#run1').on('mouseover',function(){
@@ -53,6 +57,9 @@ d3.select('#run1').on('mouseover',function(){
     .attr('d',area(data1))
     .transition()
     .duration(500)
+    myTool
+    .style("opacity","1")
+    .style("display","block");
 }) 
 
 d3.select('#run2').on('mouseover',function(){
@@ -61,6 +68,9 @@ d3.select('#run2').on('mouseover',function(){
     .attr('d',area(data2))
     .transition()
     .duration(500)
+    myTool
+    .style("opacity","1")
+    .style("display","block");
 }) 
 
 d3.select('#run3').on('mouseover',function(){
@@ -69,6 +79,7 @@ d3.select('#run3').on('mouseover',function(){
     .attr('d',area(data3))
     .transition()
     .duration(500)
+    myTool
 }) 
 
 d3.select('#run4').on('mouseover',function(){
@@ -77,6 +88,7 @@ d3.select('#run4').on('mouseover',function(){
     .attr('d',area(data4))
     .transition()
     .duration(500)
+    myTool
 }) 
 
 
